fe8d8b4408d4a1f7dd709827ef1506deaf22a980
tannerb9 <tanner-brainard@hotmail.com>
2021-03-05 09:37:07 -0600

    feat: created basic template structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -11,8 +10,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
