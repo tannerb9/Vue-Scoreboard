886b429c3137951502485202ca343534a2459780
tannerb9 <tanner-brainard@hotmail.com>
2021-03-10 11:38:25 -0600

    feat: moved initial state up to App.vue and separated into three components and passed props via App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <template>
   <div id="app">
-    <Scoreboard />
+    <Title :teams="teams" />
+    <ScoreMessage :teams="teams" />
+    <div>
+      <template v-for="team in teams">
+        <ScoreCard
+          :key="team.name"
+          :team="team"
+          @add-point="handleAddPoint"
+          @subtract-point="handleSubtractPoint"
+        />
+      </template>
+    </div>
+    <ScoreCard />
   </div>
 </template>
 
 <script>
-import Scoreboard from "./components/Scoreboard.vue";
+import Title from "./components/Title.vue";
+import ScoreMessage from "./components/ScoreMessage";
+import ScoreCard from "./components/ScoreCard.vue";
 
 export default {
   name: "App",
   components: {
-    Scoreboard,
+    Title,
+    ScoreMessage,
+    ScoreCard,
+  },
+  data() {
+    return {
+      teams: [
+        { name: "Team 1", score: 0, winning: false },
+        { name: "Team 2", score: 0, winning: false },
+      ],
+      leaderboardMessage: "",
+    };
+  },
+  methods: {
+    checkWinner() {
+      const [team1, team2] = this.teams;
+      if (team1.score > team2.score) {
+        team1.winning = true;
+        team2.winning = false;
+      } else if (team2.score > team1.score) {
+        team2.winning = true;
+        team1.winning = false;
+      } else {
+        team1.winning = false;
+        team2.winning = false;
+      }
+    },
+    handleAddPoint(team) {
+      team.score++;
+      this.checkWinner();
+    },
+    handleSubtractPoint(team) {
+      team.score !== 0 && team.score--;
+      this.checkWinner;
+    },
   },
 };
 </script>
---
+++ src/components/ScoreMessage.vue
@@ -0,0 +1,41 @@
+<template>
+  <h2>{{ message }}</h2>
+</template>
+
+<script>
+export default {
+  name: "ScoreMessage",
+  props: {
+    teams: Array,
+  },
+  methods: {
+    checkWinner() {
+      const [team1, team2] = this.teams;
+      let isPlural = true;
+
+      if (team1.score > team2.score) {
+        if (team1.score - team2.score === 1) {
+          isPlural = false;
+        }
+        return `${team1.name} is winning by ${team1.score -
+          team2.score} point${isPlural ? "s" : ""}!`;
+      } else if (team1.score < team2.score) {
+        if (team2.score - team1.score === 1) {
+          isPlural = false;
+        }
+        return `${team2.name} is winning by ${team2.score -
+          team1.score} point${isPlural ? "s" : ""}!`;
+      } else {
+        return "The game is tied!";
+      }
+    },
+  },
+  computed: {
+    message() {
+      return this.checkWinner();
+    },
+  },
+};
+</script>
+
+<style></style>
